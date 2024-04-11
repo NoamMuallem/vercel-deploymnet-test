91e947548d64112c046fbf4a45a4c88714c22b56
Noam Muallem <noammu072@gmail.com>
2024-04-11 08:10:25 +0300

    again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider>{children}</TRPCReactProvider>
-        <div>test</div>
       </body>
     </html>
   );
